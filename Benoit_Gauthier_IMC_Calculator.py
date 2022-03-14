d54a7f2203c69cd168c39003b1dce464cf2a2a62
Benoit G <94395967+zebenny9616@users.noreply.github.com>
2022-03-14 10:46:51 -0400

    Benoit_Gauthier
    Final

---
+++ Benoit_Gauthier_IMC_Calculator.py
@@ -0,0 +1,20 @@
+def calcul_imc(): 
+    grandeur_en_metre = float(input("Entrez votre grandeur en metre : "))
+    poids_en_kg = float(input("Entrez votre poids en kilogramme : "))
+
+    imc = poids_en_kg / (grandeur_en_metre)**2
+    msg = ""
+    if imc < 18.5:
+        msg = "Poids insuffisant"
+    elif 18.5 <= imc < 25 :
+        msg = "Poids santé"
+    elif 25 <=  imc < 30 :
+        msg = "Excès de poids"
+    elif 30 <= imc < 40  :
+        msg = "Obèse"
+    return msg
+
+msg = calcul_imc()
+
+print(f"Vous êtes dans la catégorie d'IMC suivante : {msg}. ")
+
